d136bbae9370c9b495fe03bcdd109c006f96d4ca
JasVv <justice1322.zetta@gmail.com>
2021-11-04 18:31:27 +0900

    Flowの基礎

--- test/akka/stream/BasicsAndWorkingWithFlowsSpec.scala
+++ test/akka/stream/BasicsAndWorkingWithFlowsSpec.scala
@@ -53,5 +53,25 @@ class BasicsAndWorkingWithFlowsSpec extends PlaySpec
       val res4: NotUsed = source.to(sink).run()
       assert(res4.isInstanceOf[NotUsed])
     }
+
+    "Flowの作成と実行" in {
+      // Flowは1つの入力と1つの出力を持つ
+      // 型パラメータ1つめは入力される値の型、2つめは出力される値の型、3つめは実行時に返すマテリアライズされた値の型
+      val flow: Flow[Int, Int, NotUsed] = Flow.fromFunction(_ * 2)
+
+      // flowは単体では実行できず、実行するときはSourceとSinkを結合する必要がある
+      val source: Source[Int, NotUsed] = Source(1 to 10)
+      val sink: Sink[Int, Future[Int]] = Sink.fold[Int, Int](0)(_ + _)
+      val runnable = source.via(flow).toMat(sink)(Keep.right)
+
+      val res1: Future[Int] = runnable.run()
+      Await.result(res1, Duration.Inf) mustBe 110
+
+      // source.via(flow).toMat(sink)(Keep.both).run()と等価
+      // よって戻り値はsourceとsinkのマテリアライズされた値のTuple
+      val res2 = flow.runWith(source, sink)
+      assert(res2._1.isInstanceOf[NotUsed])
+      Await.result(res2._2, Duration.Inf) mustBe 110
+    }
   }
 }
